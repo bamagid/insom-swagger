42f1462fa7da427d38e8e6e53c6747cfd97e2660
bamagid <81322603+bamagid@users.noreply.github.com>
2023-12-24 05:49:48 +0000

    Update insowagger.js

--- insowagger.js
+++ insowagger.js
@@ -7,76 +7,88 @@ if (!inputPath) {
   throw new Error('Missing Input Path argument!');
 }
 
+const BASE_URL = 'http://127.0.0.1:8000/';
+
 const file = JSON.parse(fs.readFileSync(inputPath, { encoding: 'utf8', flag: 'r' }));
 
-const URL_REGEXP = /\/[\w-_/]+$/;
+const URL_REGEXP = /(?:https?:\/\/[^/]+)?(\/[\w-_/]+)$/;
 
 const swagger = {
-   openapi: '3.0.0',
-    info: {
-        title: 'Your API Title',
-        description: 'Your API Description',
-        version: '1.0.0',
+  openapi: '3.0.0',
+  info: {
+    title: 'Your API Title',
+    description: 'Your API Description',
+    version: '1.0.0',
+  },
+  security: [
+    {
+      BearerAuth: [],
     },
-   security: [
-                    {
-                        BearerAuth:[]
-                    }
-                ],
-
-    components: {
-        securitySchemes: {
-            BearerAuth: {
-                type: "http",
-                bearerFormat: "JWT",
-                scheme :  "bearer"
-            }
-        }
+  ],
+  components: {
+    securitySchemes: {
+      BearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
     },
+  },
   consumes: ['application/json'],
   paths: file.resources.reduce((ac, item) => {
-    if (!item.url) return ac;
+    if (!item.url || !item.method) {
+      console.error('Skipping invalid resource:', item);
+      return ac;
+    }
 
     const match = URL_REGEXP.exec(item.url);
-    const key = match ? match[0] : '/';
+    const extractedUrl = match ? match[0] : '';
+
+    // Ajoute le chemin relatif à l'objet ac sans la base URL
+    const key = extractedUrl.slice(BASE_URL.length);
 
     ac[key] = ac[key] || {};
     item.method = item.method.toLowerCase();
 
     ac[key][item.method] = {
-      summary: item.name,
-      description: item.description,
+      summary: item.name || '',
+      description: item.description || '',
       produces: ['application/json'],
       responses: {
         200: {
-          examples: {
-            'application/json': '',
+          content: {
+            'application/json': {
+              examples: {
+                'application/json': '',
+              },
+            },
           },
+          description: 'Success',
         },
       },
-      tags: [file.resources.find((resource) => resource._id === item.parentId).name],
+      tags: [item.parentId ? file.resources.find((resource) => resource._id === item.parentId)?.name || '' : ''],
     };
 
     ac[key][item.method].parameters = [];
 
-    if (item.parameters.length) {
+    if (item.parameters && item.parameters.length) {
       ac[key][item.method].parameters.push(
         ...item.parameters.map((param) => ({
-          in: 'query',
-          name: param.name,
-          type: 'string',
+          in: 'path',
+          name: param.name || '',
+          type: param.type || 'string',
         }))
       );
     }
 
-    if (item.headers.length) {
+    if (item.headers && item.headers.length) {
       ac[key][item.method].parameters.push(
         ...item.headers
           .filter((p) => p.name !== 'Content-Type')
           .map((p) => ({
             in: 'header',
-            name: p.name,
-            type: 'string',
+            name: p.name || '',
+            type: p.type || 'string',
           }))
       );
     }
@@ -86,6 +98,7 @@ const swagger = {
       try {
         json = JSON.parse(item.body.text);
       } catch (error) {
+        console.error('Error parsing JSON for resource:', item);
         return ac;
       }
       ac[key][item.method].requestBody = {
@@ -101,7 +114,7 @@ const swagger = {
                       case 'number':
                         return 'integer';
                       default:
-                        return typeof v;
+                        return { type: typeof v };
                     }
                   })(),
                 }),
@@ -119,3 +132,6 @@ const swagger = {
 };
 
 fs.writeFileSync(outputPath, JSON.stringify(swagger, null, 4));
+console.log(
+    `Documentation Swagger générées et écrites dans le fichier json avec succès: ${outputPath}`
+  );
